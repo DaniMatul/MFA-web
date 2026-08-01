03f170fa4b9467a9020ead444e7d9f807ee17bd0
Daniela Matul <mayda.matul@gmail.com>
2026-08-01 16:36:19 -0600

    feat: create MFA structure and example

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,7 +1,19 @@
+import './MFA.css'
 export default function Home() {
   return (
     <>
-    <h1>MFA</h1>
+    <div className="mfa-general-container">
+      <div className="img-container">
+        {/* Aqui las imagenes van cambiando dependiendo el paso */}
+        <img src="../paso1.webp" alt="p" />
+      </div>
+      <div className="inputs-container">
+        {/* Aqui van los inputs de cada paso */}
+        {/* PASO 3 - Solicitar token*/}
+        {/* PASO 4 - Solicitar y validar biometrico */}
+        <p>Aqui van los inputs de cada paso</p>
+      </div>
+    </div>
     </>
   );
 }
