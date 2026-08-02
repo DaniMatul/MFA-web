b84a8db008f5eb42090319fff645d70ab64bd73a
dalbizurez <dalbizurez@correo.url.edu.gt>
2026-08-02 14:13:35 -0600

    feat: database connection

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from typing import Annotated
+from pydantic import BaseModel
+
+import asyncpg
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 from app.users.routes import router as userRouter
 app = FastAPI()
-
 app.include_router(userRouter)
+
+
+async def connect_db():
+    return await asyncpg.connect(DATABASE_URL)
+
