84e64ddb6cc6eaedd1d8c35d78952ca8419e8b38
Daniela Matul <mayda.matul@gmail.com>
2026-08-02 12:09:02 -0600

    feat: add logic to generate, send and validate token

---
+++ backend/app/token/controller.py
@@ -0,0 +1,46 @@
+import secrets
+from fastapi import HTTPException
+from passlib.context import CryptContext
+
+from app.token.sendGrid import sendEmail
+
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+def sendVerificationCode():
+    try: 
+        # Obtenemos info de usuario de bd (HARCODEADO)
+        email = 'mayda.matul@gmail.com'
+
+        # Primero obtenemos token
+        #code = "".join(secrets.choice("0123456789") for _ in range(6)) listo
+        token = "123456"
+        print(token) 
+
+        # Hasheamos
+        hashedToken = pwd_context.hash(token)
+        print("hash")
+        print(hashedToken)
+
+        # Guardamos token en bd (PENDIENTE)
+
+        # Enviamos token al servicio (LISTO)
+        # email = sendEmail(email, token)
+
+        return {"message": "Verification code sent", "email": email}
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error)) from error
+
+def validateVerificationToken(token):
+    # Aqui obtenemos al usuairo usando correo
+    # Obtener desde bd pero harcodeado para la prueba
+    hashedToken = '$2b$12$POFLRAAIKqq.QtgtodfPg.bwK2QxvFPPnZAFDwtp20Kcach82ScKG'
+    is_valid = pwd_context.verify(
+        token,
+        hashedToken
+    )
+    return is_valid
+
---
+++ backend/app/token/routes.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from app.token.controller import sendVerificationCode, validateVerificationToken
+
+router = APIRouter(
+    prefix="/token",
+    tags=["token"]
+)
+
+@router.get("/send-verification-token")
+async def sendSms():
+    rersult = sendVerificationCode()
+    return rersult
+
+@router.post("/validate-token")
+async def validateToken(token):
+    rersult = validateVerificationToken(token)
+    return rersult
